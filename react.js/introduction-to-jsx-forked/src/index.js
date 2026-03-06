20cd9e781ebe31bd380854bbeddb450b1c503492
David Aning Amponsah <amponsahaning@gmail.com>
2026-03-06 07:43:36 -0800

    Add files via upload

---
+++ react.js/introduction-to-jsx-forked/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>
+      Welcome to react.js React is a javascript library for building user
+      interface
+    </p>
+  </div>,
+  document.getElementById("root")
+);
