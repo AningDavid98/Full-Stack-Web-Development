172988509a9b5df08797ef37ecf0ebe5c77f812f
David Aning Amponsah <amponsahaning@gmail.com>
2026-03-09 15:37:29 -0700

    Add files via upload

---
+++ react.js/inline-styling-in-jsx-forked/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const randomStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+};
+
+randomStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={randomStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
